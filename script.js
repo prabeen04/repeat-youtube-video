bc67ba303aa61aa64c52477e0f278bc8693e919c
Prabeen04 <prabeen.strange@gmail.com>
2019-10-19 22:09:32 +0530

    playVideo function created

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 (function() {
   let videoPlayer = document.getElementsByName("video")[0];
-  videoPlayer.onpause(e => videoPlayer.play());
+  videoPlayer.onpause(e => {
+    alert("video paused");
+    playVideo(videoPlayer);
+  });
 })();
+
+function playVideo(instance) {
+  if (!instance) return;
+  instance.play();
+}
