bdcf66f2e6ef8b11a08a1bd94f318c6f48d5dc51
Prabeen04 <prabeen.strange@gmail.com>
2019-10-20 20:01:48 +0530

    alerts removed

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 (function() {
-  alert("initialized");
   let videoPlayer = document.getElementsByTagName("video")[0];
-  debugger;
   if (!videoPlayer) return;
-  if (videoPlayer) {
-    alert(JSON.stringify(videoPlayer));
-  }
+  
   videoPlayer.onpause(e){
     alert("video paused");
     playVideo(videoPlayer);
