b30b6eed91e327d34e974a4e79a23f894f76d300
Prabeen04 <prabeen.strange@gmail.com>
2019-11-22 22:28:22 +0530

    readyState checked

--- script.js
+++ script.js
@@ -3,9 +3,19 @@
     if (!instance) return;
     instance.play();
   }
+
+  // startBtn.onclick = () => {
+  //   startBtn.innerText = "Stop";
+  // };
   let videoPlayer = document.getElementsByTagName("video")[0];
   if (!videoPlayer) return;
   debugger;
+  if (videoPlayer.onended) {
+    alert("ended");
+  }
+  if (videoPlayer.readyState === 0) {
+    alert("stopped");
+  }
   videoPlayer.onpause = () => {
     playVideo(videoPlayer);
   };
