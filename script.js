e56343e65d36a6bf0fe4e18f2cbb5adcb6f7757c
Prabeen04 <prabeen.strange@gmail.com>
2019-11-23 19:40:55 +0530

    video restart after  stopped successfull

--- script.js
+++ script.js
@@ -3,20 +3,12 @@
     if (!instance) return;
     instance.play();
   }
-
-  // startBtn.onclick = () => {
-  //   startBtn.innerText = "Stop";
-  // };
   let videoPlayer = document.getElementsByTagName("video")[0];
   if (!videoPlayer) return;
-  debugger;
-  if (videoPlayer.onended) {
-    alert("ended");
-  }
-  if (videoPlayer.readyState === 0) {
-    alert("stopped");
-  }
+
   videoPlayer.onpause = () => {
-    playVideo(videoPlayer);
+    if (videoPlayer.ended) {
+      playVideo(videoPlayer);
+    }
   };
 })();
